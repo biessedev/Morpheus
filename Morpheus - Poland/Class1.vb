b13648e9c43d5bc8838ee35b95fce7e17868f5ad
BSSoper <BSSoper@BSS-PC>
2016-11-08 13:51:02 +0200

    updated Class1.vb

--- Morpheus - Poland/Class1.vb
+++ Morpheus - Poland/Class1.vb
@@ -21,3 +21,35 @@
 
 
 End Class
+
+
+Class ListViewItemComparerAscDesc
+    Implements IComparer 
+    Private col As Integer
+    Private order as SortOrder
+    
+    Public Sub New()
+        col = 0
+        order = SortOrder.Ascending
+    End Sub
+    
+    Public Sub New(column As Integer, order as SortOrder)
+        col = column
+        Me.order = order
+    End Sub
+    
+    Public Function Compare(x As Object, y As Object) As Integer _
+                        Implements System.Collections.IComparer.Compare
+        Dim returnVal as Integer = -1
+        returnVal = [String].Compare(CType(x, _
+                        ListViewItem).SubItems(col).Text, _
+                        CType(y, ListViewItem).SubItems(col).Text)
+        ' Determine whether the sort order is descending.
+        If order = SortOrder.Descending Then
+            ' Invert the value returned by String.Compare.
+            returnVal *= -1
+        End If
+
+        Return returnVal
+    End Function
+End Class
