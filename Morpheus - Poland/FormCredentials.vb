4ef2f40bfae3e7145248fc4f20c63d85781f9986
natalia <natalia@HOME-PC>
2017-01-20 13:09:21 +0200

    Feature #7346 - Default tag for connection dropdown list

--- Morpheus - Poland/FormCredentials.vb
+++ Morpheus - Poland/FormCredentials.vb
@@ -68,7 +68,21 @@ Public Class FormCredentials
                 ComboBoxHost.Items.Add(conn.Name & " - " & builder("host"))
             End If
         Next
-        ComboBoxHost.SelectedIndex = 0
+        Dim index = 0
+        For Each item as ConnectionStringSettings in ConfigurationManager.ConnectionStrings
+            builder.ConnectionString = ConfigurationManager.ConnectionStrings(item.Name).ConnectionString
+            Try
+                If(builder("default") = "true" and builder("connectionType") = "MainConnections") Then
+                    index = ComboBoxHost.FindStringExact(item.Name & " - " & builder("host"))
+                    ComboBoxHost.SelectedIndex = index
+                    Exit For
+                End If
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        ComboBoxHost.SelectedIndex = index
         TextBoxUserName.Text = ""
         TextBoxPassword.Text = ""
         LabelHost.Text = "Host: " '& builder("host")
